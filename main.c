0bd8546baf833df78a9a675c5d72cb31adaa7da6
KKamiyasan7 <vinzarch7@gmail.com>
2025-04-25 13:26:13 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #ifdef __linux
 
 #define _GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +9,20 @@
 #include <string.h>
 #include <fcntl.h>
 
+void handler(int signal){ // Just handler if the length is over
+    printf("Signal %d recieved\n",signal);
+    fflush(stdout);
+    exit(0);
+}
+
 int main(int argc,char **argv){
+    struct sigaction sig;
+    sig.sa_flags = SA_RESTART;
+    sig.sa_handler = handler;
+    sigemptyset(&sig.sa_mask);
+    sigaction(SIGINT,&sig,NULL);
+    sigaction(SIGSEGV,&sig,NULL);
+
     if(argc < 2){
         printf("Error Argument\n");
         return 1;
